35a820d4ddf8729ed15d601f4a98e54f57f30f59
SHILI Oussama <oussama.shili@esprit.tn>
2018-10-15 23:01:32 +0100

    changes for hosted version

--- station-f-frontend/src/app/utils/globals.ts
+++ station-f-frontend/src/app/utils/globals.ts
@@ -1,4 +1,5 @@
 'use strict';
 
 export const separator = '/';
-export const URL = 'http://localhost:3000';
+// export const URL = 'http://localhost:3000';
+export const URL = 'ec2-176-34-131-11.eu-west-1.compute.amazonaws.com:8081';
